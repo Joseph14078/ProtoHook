312fd555136167ae738a084aa17db80d6c3bb5bd
Joseph Judge <joseph14078@gmail.com>
2019-01-22 20:08:25 -0500

    Can now spawn multiple PH instances Can properly bind to instances Hid hook function Added unhooking Fixed unbinding Fixed all the stuff I broke while I was half awake

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-var PH = function(ref, func) {
-    if (typeof func != "undefined")
-        return PH.hook(ref, func);
-
-    var protoKeys = Object.keys(ref.prototype);
-
-    for (var i = 0; i < protoKeys.length; i++) {
-        var key = protoKeys[i];
-        PH.hook(ref, key);
-    }
+var _PHGroup = function() {
+    this._bindNum = 0;
+    this._binds = new Map();
 };
 
-PH._run = function(ref, funcKey, step,  obj, args, result) {
-    if (typeof PH._binds.get(ref) == 'undefined') return;    
-    var refBinds = PH._binds.get(ref);
+_PHGroup.prototype._run = function(ref, funcKey, step,  obj, args, result) {
+    if (typeof this._binds.get(ref) == 'undefined') return;    
+    var refBinds = this._binds.get(ref);
     
     if (typeof refBinds[funcKey] == 'undefined') return;
     var funcBinds = refBinds[funcKey];
@@ -27,15 +20,17 @@ PH._run = function(ref, funcKey, step,  obj, args, result) {
     }
 };
 
-PH._bindNum = 0;
-PH._binds = new Map();
+_PHGroup.prototype.bind = function(ref, func, step, callback) {
+    var funcAndKey = this._findFuncAndKey(ref, func);
+    func = funcAndKey[0];
+    var funcKey = funcAndKey[1];
 
-PH.bind = function(ref, func, step, callback) {
-    if (typeof PH._binds.get(ref) == 'undefined')
-        PH._binds.set(ref, {});
-    var refBinds = PH._binds.get(ref);
+    if (!func._PHOriginal) this._hook(ref, funcKey);
 
-    var funcKey = PH._getFuncKey(ref, func);
+    if (typeof this._binds.get(ref) == 'undefined')
+        this._binds.set(ref, {});
+    var refBinds = this._binds.get(ref);
+    
     if (typeof refBinds[funcKey] == 'undefined')
         refBinds[funcKey] = {};
     var funcBinds = refBinds[funcKey];
@@ -45,17 +40,20 @@ PH.bind = function(ref, func, step, callback) {
     var stepBinds = funcBinds[step];
     step = step || 'end';
 
-    stepBinds[PH._bindNum] = callback;
+    stepBinds[this._bindNum] = callback;
 
-    return PH._bindNum++;
+    return this._bindNum++;
 };
 
-PH.unbind = function(ref, func, step, bindNum) {
-    if (typeof PH._binds.get(ref) == 'undefined')
-        PH._binds.set(ref, {});
-    var refBinds = PH._binds.get(ref);
+_PHGroup.prototype.unbind = function(ref, func, step, bindNum) {
+    var funcAndKey = this._findFuncAndKey(ref, func);
+    func = funcAndKey[0];
+    var funcKey = funcAndKey[1];
 
-    var funcKey = PH._getFuncKey(ref, func);
+    if (typeof this._binds.get(ref) == 'undefined')
+        this._binds.set(ref, {});
+    var refBinds = this._binds.get(ref);
+
     if (typeof refBinds[funcKey] == 'undefined')
         refBinds[funcKey] = {};
     var funcBinds = refBinds[funcKey];
@@ -69,47 +67,121 @@ PH.unbind = function(ref, func, step, bindNum) {
     if (Object.keys(stepBinds).length == 0)
         delete funcBinds[step];
 
-    if (Object.keys(funcBinds).length == 0)
+    if (Object.keys(funcBinds).length == 0) {
         delete refBinds[funcKey];
+        this._unhook(ref, func);
+    }
 
     if (Object.keys(refBinds).length == 0)
-        delete PH._binds.delete(ref);
+        delete this._binds.delete(ref);
 };
 
-PH._getFuncKey = function(ref, func) {
+_PHGroup.prototype._findKey = function(obj, item) {
+    if (typeof item == "string") return item;
+
+    var objKeys = Object.keys(obj);
+    for (var i = 0; i < objKeys.length; i++) {
+        var key = objKeys[i];
+        if (obj[key] === item)
+            return key;
+    }
+};
+
+_PHGroup.prototype._isProto = function(ref) {
+    return typeof ref == "function";
+};
+
+_PHGroup.prototype._findFuncKey = function(ref, func) {
     if (typeof func == "string") return func;
 
-    var funcKey;
-    var protoKeys = Object.keys(ref.prototype);
+    var key;
+    var isProto = this._isProto(ref);
 
-    for (var i = 0; i < protoKeys.length; i++) {
-        var key = protoKeys[i];
-        if (ref.prototype[key] === func) {
-            funcKey = key;
-            break;
-        }
+    // Find key from function (more expensive)
+    if (!isProto) {
+        key = this._findKey(ref, func);
+        if (typeof key != "undefined") return key;
+
+        key = this._findKey(ref.constructor.prototype, func);
+        if (typeof key != "undefined") return key;
+    } else {
+        key = this._findKey(ref.prototype, func);
+        if (typeof key != "undefined") return key;
     }
-    return funcKey;
-}
+};
 
-PH.hook = function(ref, func) {
+_PHGroup.prototype._findFunc = function(ref, key) {
+    if (typeof key == "function") return key;
+
+    var func;
+    var isProto = this._isProto(ref);
+
+    if (!isProto) {
+        func = ref[key];
+        if (typeof func != "undefined") return func;
+        
+        func = ref.constructor.prototype[key];
+        if (typeof func != "undefined") return func;
+    } else {
+        func = ref.prototype[key];
+        if (typeof func != "undefined") return func;
+    }
+};
+
+_PHGroup.prototype._findFuncAndKey = function(ref, func) {
     var funcKey;
     if (typeof func == "string") {
         funcKey = func;
-        func = ref.prototype[funcKey];
-    } else funcKey = PH._getFuncKey(ref, func);
-    if (!funcKey) return;
-
-    if (ref.prototype[funcKey]._isPH) return;
-
-    ref.prototype[funcKey] = function() {
-        PH._run(ref, funcKey, 'start', this, arguments, undefined);
-        var result = func.apply(this, arguments);
-        PH._run(ref, funcKey, 'end', this, arguments, result);
+        func = this._findFunc(ref, funcKey);
+        if (!func) throw "Could not find function in ref.";
+    } else {
+        funcKey = this._findFuncKey(ref, func);
+        if (!funcKey) throw "Could not find function key in ref.";
     }
-
-    ref.prototype[funcKey]._isPH = true;
+    return [func, funcKey];
 };
+
+_PHGroup.prototype._getNewFuncDest = function(ref) {
+    return this._isProto(ref) ? ref.prototype : ref;
+}
+
+_PHGroup.prototype._hook = function(ref, func) {
+    var newFuncDest = this._getNewFuncDest(ref);
+    var funcAndKey = this._findFuncAndKey(ref, func);
+    func = funcAndKey[0];
+    var funcKey = funcAndKey[1];
+    
+    if (newFuncDest[funcKey]._PHOriginal) return;
+    
+    var phGroup = this;
+
+    // Replacement will always be stored in ref rather than its prototype
+    newFuncDest[funcKey] = function() {
+        phGroup._run(ref, funcKey, 'start', this, arguments, undefined);
+        var result = func.apply(this, arguments);
+        phGroup._run(ref, funcKey, 'end', this, arguments, result);
+    };
+    newFuncDest[funcKey]._PHOriginal = func;
+};
+
+_PHGroup.prototype._unhook = function(ref, func) {
+    var newFuncDest = this._getNewFuncDest(ref);
+    var funcAndKey = this._findFuncAndKey(ref, func);
+    func = funcAndKey[0];
+    var funcKey = funcAndKey[1];
+    
+    if (!newFuncDest[funcKey]._PHOriginal) return;
+
+    var funcFromConstructor = !this._isProto(ref) && (ref.constructor.prototype[funcKey] === newFuncDest[funcKey]._PHOriginal);
+
+    if (funcFromConstructor)
+        delete newFuncDest[funcKey];
+    else
+        newFuncDest[funcKey] = newFuncDest[funcKey]._PHOriginal;
+};
+
+var PH = new _PHGroup();
+PH.Group = _PHGroup();
 
 // UMD (Universal Module Definition)
 (function(root) {
